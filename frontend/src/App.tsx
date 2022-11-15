6de7ed2d61f90daccc8501bb1b2f37dcd4ecac60
jminango20 <jcarlosminango@gmail.com>
2022-11-14 21:24:20 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import { Component } from "react"
+import NotificationButton from "./assets/components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Hello World</h1>
+    <NotificationButton />
+    </>
   )
 }
 
